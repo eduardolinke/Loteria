4377ba7721df4d7792dbb4b106361b3d753d5f43
TDS2019 <tds2019@gmail.com>
2019-08-29 21:50:15 -0300

    Aceitando agora números validos

--- Loteria/src/loteria/Loteria.java
+++ Loteria/src/loteria/Loteria.java
@@ -9,13 +9,25 @@ public class Loteria {
         Scanner in = new Scanner(System.in);
         System.out.println("Digite 6 Números");
         for (int i = 0; i < 6; i++) {
-            capNum(in, i);
-
+           int num = -1;
+            while (num == -1) {                
+                num = 80
+                        capNum(in, i);
+            }
         }
     }
 
     public static int capNum(Scanner in, int i) {
         System.out.println("Digite o " + (i + 1) + "º Número");
-        return 0;
+        int num = in.nextInt();
+        if (num >= 1 && num <= 60) {
+            return num;
+        } else {
+            return -1;
+        }
+
+    
+    
     }
+    
 }
