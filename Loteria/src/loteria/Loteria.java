6f0f1bcd80ddb95158b4f25515caade855a7d366
TDS2019 <tds2019@gmail.com>
2019-08-29 22:19:56 -0300

    Criada Array para pegar 6 valores

--- Loteria/src/loteria/Loteria.java
+++ Loteria/src/loteria/Loteria.java
@@ -1,4 +1,3 @@
-
 package loteria;
 
 import java.util.Scanner;
@@ -6,13 +5,24 @@ import java.util.Scanner;
 public class Loteria {
 
     public static void main(String[] args) {
+        int[] valores = new int[]{0, 0, 0, 0, 0, 0};
+        imprimeVetor(valores);
+//       int[] valores2;
+//       valores2 = new int [6];
+//       valores2[0] = 0;
+//       valores2[1] = 0;
+//       valores2[2] = 0;
+//       valores2[3] = 0;
+//       valores2[4] = 0;
+//       valores2[5] = 0;
+//       
+
         Scanner in = new Scanner(System.in);
         System.out.println("Digite 6 Números");
         for (int i = 0; i < 6; i++) {
-           int num = -1;
-            while (num == -1) {                
-                num = 80
-                        capNum(in, i);
+            int num = -1;
+            while (num == -1) {
+                num = capNum(in, i);
             }
         }
     }
@@ -24,10 +34,16 @@ public class Loteria {
             return num;
         } else {
             return -1;
+
         }
 
-    
-    
     }
-    
+
+    public static void imprimeVetor(int[] vet) {
+        for (int i = 0; i < vet.length; i++) {
+            System.out.print(vet[i]+" " );    
+        }
+        System.out.println("");
+    }
+
 }
